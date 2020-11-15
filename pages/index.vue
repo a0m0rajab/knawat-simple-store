4dda2768d6eea5c6e9510b6add9f4a97e2a9186b
abdurrahman rajab <rajab-2@windowslive.com>
2020-11-16 02:50:04 +0300

    feat(index): connecting the index page with store
    Adding the needed information for index page to connect with store

--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,40 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+<v-row justify="center" align="center">
+    <v-col v-for="product in products" :key="product.sku">
+        <product :title="product.name[`en`]" :desc="product.description[`en`]" :link="porduct.images[0]" :price="product.variations[0].cost_price" />
     </v-col>
-  </v-row>
+    <v-col>
+        <mountain />
+    </v-col>
+</v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Logo from "~/components/Logo.vue";
+import mountain from "~/components/mountain.vue";
+
+import product from "~/components/product.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import {
+    mapMutations,
+    mapState,
+    mapActions
+} from "vuex";
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+    components: {
+        mountain,
+        Logo,
+        VuetifyLogo,
+        product,
+    },
+    created() {
+        console.log("created");
+        this.$store.dispatch("products/getAllProducts");
+    },
+    computed: mapState({
+        // products: (state) => state.products.all,
+    }),
+    methods: mapActions("cart", ["addProductToCart"]),
+};
 </script>

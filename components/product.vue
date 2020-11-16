c548939f45677962e1561fe75eb78a313f76a581
abdurrahman rajab <rajab-2@windowslive.com>
2020-11-16 21:34:19 +0300

    UI(product): Fixing design add store logic

--- components/product.vue
+++ components/product.vue
@@ -1,24 +1,29 @@
 <template>
   <v-card class="mx-auto" max-width="400">
-    <v-img class="align-end" :src="link">
-      <v-card-title>{{ title }}</v-card-title>
-    </v-img>
+    <v-img class="align-end" :src="link"> </v-img>
+    <v-card-title>{{ title }}</v-card-title>
     <v-card-subtitle class="pb-0"> Price: {{ price }} </v-card-subtitle>
     <v-card-text v-html="desc" class="text--primary"> </v-card-text>
     <v-card-actions>
       <v-spacer />
-      <v-btn color="blue" text> Add <v-icon>mdi-plus</v-icon> </v-btn>
+      <v-btn @click="addProductToCart(sku)" color="blue" text>
+        Add <v-icon>mdi-plus</v-icon>
+      </v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
   props: {
     title: String,
     desc: String,
     link: String,
     price: Number,
+    sku: String,
   },
+  methods: mapActions("cart", ["addProductToCart"]),
 };
 </script>

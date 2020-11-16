7349d0ae456680a2a000adb031779734513c21a2
abdurrahman rajab <rajab-2@windowslive.com>
2020-11-16 16:05:03 +0300

    UI(product): Minor fix

--- components/product.vue
+++ components/product.vue
@@ -1,27 +1,24 @@
 <template>
-<v-card class="mx-auto" max-width="400">
-    <v-img class="white--text align-end" :src="link">
-        <v-card-title>{{ title }}</v-card-title>
+  <v-card class="mx-auto" max-width="400">
+    <v-img class="align-end" :src="link">
+      <v-card-title>{{ title }}</v-card-title>
     </v-img>
-    <v-card-subtitle class="pb-0"> {{ price }} </v-card-subtitle>
-    <v-card-text class="text--primary">
-        {{ desc }}
-    </v-card-text>
+    <v-card-subtitle class="pb-0"> Price: {{ price }} </v-card-subtitle>
+    <v-card-text v-html="desc" class="text--primary"> </v-card-text>
     <v-card-actions>
-        <v-spacer />
-        <v-btn color="blue" text> Add <v-icon>mdi-plus</v-icon>
-        </v-btn>
+      <v-spacer />
+      <v-btn color="blue" text> Add <v-icon>mdi-plus</v-icon> </v-btn>
     </v-card-actions>
-</v-card>
+  </v-card>
 </template>
 
 <script>
 export default {
-    props: {
-        title: String,
-        desc: String,
-        link: String,
-        price: Number,
-    },
+  props: {
+    title: String,
+    desc: String,
+    link: String,
+    price: Number,
+  },
 };
 </script>

75be5a75d8aea6b4437954fe0e360c0821a9c478
abdurrahman rajab <rajab-2@windowslive.com>
2020-11-16 21:35:45 +0300

    UI(productPage) adding pagination - store logic
    adding the pagination and store logic to index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,39 +3,61 @@
     <v-col v-for="product in products" :key="product.sku">
       <product
         :title="product.name.tr"
-        :desc="product.description.tr"
-        :link="product.images[0]"
-        :price="product.variations[0].market_price"
+        :link="imageName(product.images[0], product.name.tr)"
+        :price="product.variations.market_price"
+        :sku="product.sku"
       />
     </v-col>
-    <v-col>
-      <mountain />
-    </v-col>
+    <div class="text-center">
+      <v-pagination
+        v-model="page"
+        :length="count / 10"
+        :total-visible="9"
+      ></v-pagination>
+    </div>
   </v-row>
 </template>
 
+
+
 <script>
 import Logo from "~/components/Logo.vue";
-import mountain from "~/components/mountain.vue";
 
 import product from "~/components/product.vue";
 import VuetifyLogo from "~/components/VuetifyLogo.vue";
-import { mapMutations, mapState, mapActions } from "vuex";
+import { mapMutations, mapState, mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
-    mountain,
     Logo,
     VuetifyLogo,
     product,
   },
+  data() {
+    return {
+      page: 1,
+    };
+  },
+  watch: {
+    page: function () {
+      this.$store.dispatch("products/getAllProducts", this.page);
+    },
+  },
   created() {
     console.log("created");
-    this.$store.dispatch("products/getAllProducts");
+    this.$store.dispatch("products/getAllProducts", this.page);
   },
-  computed: mapState({
-    products: (state) => state.products.all,
-  }),
-  methods: mapActions("cart", ["addProductToCart"]),
+  computed: {
+    ...mapState({
+      products: (state) => state.products.all,
+      count: (state) => state.products.count,
+    }),
+  },
+  methods: {
+    ...mapActions("cart", ["addProductToCart"]),
+    imageName(link, name) {
+      return `${link}/${name}/200`;
+    },
+  },
 };
 </script>

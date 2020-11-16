39d8b33a17a6258cbfeabfe47eca52afb92bdc55
abdurrahman rajab <rajab-2@windowslive.com>
2020-11-16 21:34:45 +0300

    UI(navbar) adding badge logic - store

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,46 @@
 <template>
-<v-app dark>
+  <v-app dark>
     <v-app-bar fixed app>
-        <v-toolbar-title v-text="title" />
-        <v-spacer />
-        <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-            <v-icon>mdi-cart-outline</v-icon>
-        </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn :disabled="!productAmount" icon to="/cartPage">
+        <v-badge
+          v-if="productAmount"
+          :content="productAmount"
+          :value="productAmount"
+          color="green"
+          left
+          bottom
+          to="/cartPage"
+        >
+        </v-badge>
+        <v-icon large>mdi-cart-outline</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
-        <v-container>
-            <nuxt />
-        </v-container>
+      <v-container>
+        <nuxt />
+      </v-container>
     </v-main>
     <v-footer absolute app>
-        <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
-</v-app>
+  </v-app>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-    data() {
-        return {
-            title: "Knawat Demo Store",
-        };
-    },
+  data() {
+    return {
+      title: "Knawat Demo Store",
+    };
+  },
+  computed: {
+    ...mapGetters({
+      productAmount: "cart/productAmount",
+    }),
+  },
 };
 </script>

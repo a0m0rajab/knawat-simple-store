1001bb28aef7fb59cac671567bd168ef4ea71d11
abdurrahman rajab <rajab-2@windowslive.com>
2020-11-17 20:15:07 +0300

    chore(github) adding github pages code

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  router: {
+    base: '/knawat-simple-store/'
+  },
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - knwat-simple-store',
